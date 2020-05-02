66090dc692be25001071ec71b3ec9e34f7e3ca3b
Albert Balbon <ajbalbon@gmail.com>
2020-05-02 01:00:36 -0700

    started working on working memory and fms

--- farm.js
+++ farm.js
@@ -66,15 +66,6 @@ class Nature {
         }
     }
 
-    updatePlantGrowthCycle(plant) {
-        if(this.sky === "cloudy") {
-            return false; // // delay growth
-        }
-        else {
-            
-        }
-    }
-
     updatePlantColor(plant) {
 
     }
@@ -84,17 +75,9 @@ class Nature {
             console.log("It's raining, waterReserve++")
             plant.waterReserve++;
         } else {
-            console.log()
+            console.log();
         }
 
-    }
-
-    updatePlantFruitingState(plant) {
-
-    }
-
-    decrementWaterReserve(plant) {
-        plant.waterReserve--;
     }
     
 }
@@ -104,8 +87,12 @@ plantType       : apple, corn, berry
 waterReserve    : 0 to 3
 growthCycle     : seed, stalk, bush, flower
                 : each plantType will have a certain day at which it will progress to the next stage of growth
-fruitColor      : green, yellow, black, no color
+fruitingState      : green, yellow, black, no color
 fertilizer      : True or False
+taskDone        : watering, soaped, harvesting
+age             : how many days its been
+plantBlight     : blight type
+blight          : true or false
 x               : x-coord
 y               : y-coord
 *******************************************************************************************************************/
@@ -113,18 +100,135 @@ class Plant {
     constructor() {
         this.plantType = "apple";
         this.growthCycle = "seed";
-        this.plantColor = "green";
         this.waterReserve = 0;
         this.fruitingState = null;
-        this.fertilizer = false;
+        this.fertilized = false;
         this.plantBlight = null;
+        this.blight = false;
+        this.taskDone = null;
+        this.age = 0;
     }
 
-    isHarvestable() {
-        if(this.fruitingState === "red")
-            return true
-        return false;
+    incrementAge() {
+        this.age++;
     }
+
+    decrementAge() {
+        this.age--;
+    }
+
+    incrementWaterReserve() {
+        if(this.taskDone === "watering") {
+            waterReserve++;
+            console.log("plant watered")
+        }
+    }
+
+    fixBlight() {
+        if(this.taskDone === "soaping") {
+            this.blight = null;
+            console.log("blight fixed")
+        }
+    }
+
+    addFertilizer() {
+        if(this.taskDone === "fertilize") {
+            this.fertilized = true;
+            console.log("plant fertilized")
+        }
+    }
+
+    updateGrowthCycle() {
+        // Apple growth cycle
+        if(this.plantType === "apple" && this.waterReserve > 0 && this.waterReserve <= 3 && this.age > 1 && this.growthCycle === "seed" && this.blight === null) {
+            this.growthCycle = "stalk"
+            console.log("apple seed is now a stalk")
+        }
+        if(this.plantType === "apple" && this.waterReserve > 0 && this.waterReserve <= 3 && this.age > 4 && this.growthCycle === "stalk" && this.blight === null) {
+            this.growthCycle = "bush"
+            console.log("apple stalk is now a bush")
+        }
+        if(this.plantType === "apple" && this.waterReserve > 0 && this.waterReserve <= 3 && this.age > 10 && this.growthCycle === "bush" && this.blight === null) {
+            this.growthCycle = "flower"
+            console.log("apple bush is now a flower")
+        }
+        if(this.plantType === "apple" && this.waterReserve > 0 && this.waterReserve <= 3 && this.age > 13 && this.growthCycle === "flower" && this.blight === null) {
+            this.growthCycle = "fruiting"
+            this.fruitingState = "green"
+            console.log("apple flower is now fruiting")
+        }
+
+        // Berry growth cycle
+        if(this.plantType === "berry" && this.waterReserve > 0 && this.waterReserve <= 3 && this.age > 1 && this.growthCycle === "seed" && this.blight === null) {
+            this.growthCycle = "stalk"
+            console.log("berry seed is now a stalk")
+        }
+        if(this.plantType === "berry" && this.waterReserve > 0 && this.waterReserve <= 3 && this.age > 4 && this.growthCycle === "stalk" && this.blight === null) {
+            this.growthCycle = "bush"
+            console.log("berry stalk is now bush")
+        }
+        if(this.plantType === "berry" && this.waterReserve > 0 && this.waterReserve <= 3 && this.age > 8 && this.growthCycle === "bush" && this.blight === null) {
+            this.growthCycle = "flower"
+            console.log("berry bush is now flower")
+        }
+        if(this.plantType === "berry" && this.waterReserve > 0 && this.waterReserve <= 3 && this.age > 10 && this.growthCycle === "flower" && this.blight === null) {
+            this.growthCycle = "fruiting"
+            this.fruitingState = "green"
+            console.log("berry flower is now fruiting")
+        }
+
+        // Corn growth cycle
+        if(this.plantType === "corn" && this.waterReserve > 0 && this.waterReserve <= 3 && age > 1 && this.growthCycle === "seed" && this.blight === null) {
+            this.growthCycle = "stalk"
+            console.log("corn seed is now stalk")
+        }
+        if(this.plantType === "corn" && this.waterReserve > 0 && this.waterReserve <= 3 && age > 3 && this.growthCycle === "stalk" && this.blight === null) {
+            this.growthCycle = "bush"
+            console.log("corn stalk is now bush")
+        }
+        if(this.plantType === "corn" && this.waterReserve > 0 && this.waterReserve <= 3 && age > 5 && this.growthCycle === "bush" && this.blight === null) {
+            this.growthCycle = "flower"
+            console.log("corn bush is now flower")
+        }
+        if(this.plantType === "corn" && this.waterReserve > 0 && this.waterReserve <= 3 && age > 7 && this.growthCycle === "flower" && this.blight === null) {
+            this.growthCycle = "fruiting"
+            this.fruitingState = "green"
+            console.log("corn flower is now fruiting")
+        }
+    }
+
+    updateFruitingState() {
+        // Apple Fruiting State
+        if(this.plantType === "apple" && this.waterReserve > 0 && this.waterReserve < 3 && age > 15 && this.fruitingState === "green" && this.blight === null) {
+            this.fruitingState = "red"
+            console.log("apple is now harvestable")
+        }
+
+        // Berry fruitying state
+        if(this.plantType === "berry" && this.waterReserve > 0 && this.waterReserve < 3 && age > 11 && this.fruitingState === "green" && this.blight === null) {
+            this.fruitingState = "red"
+            console.log("berry is now harvestable")
+        }
+
+        // Corn fruiting state
+        if(this.plantType === "corn" && this.waterReserve > 0 && this.waterReserve < 3 && age > 8 && this.fruitingState === "green" && this.blight === null) {
+            this.fruitingState = "red"
+            console.log("corn is now harvestable")
+        }
+    }
+
+    decrementFruitColor() {
+        if(this.fruitingState === "red") {
+            this.fruitingState = "green"
+        }
+        if(this.fruitingState === "green") {
+            this.fruitingState = "yellow";
+        }
+        if(this.fruitingState === "yellow") {
+            this.fruitingState = "black";
+        }
+    }
+
 }
 
 /*******************************************************************************************************************
@@ -137,6 +241,7 @@ class Plot {
         this.x = x;
         this.y = y;
         this.plant = null;
+        this.hasPlot = null;
     }
 }
 
@@ -208,19 +313,80 @@ class FMS {
         this.taskCount = 0;
         this.day = dayCount;
     }
+
+    addTasks(task) {
+        this.taskList.push(task);
+        this.taskCount++;
+    }
+
+    newDay() {
+        if(this.taskCount === 200) {
+            dayCount++;
+        }
+    }
 }
 
 class WorkingMem{
-    constructor(nature, fms) {
+    constructor(nature, fms, validPlots) {
+        this.plot = 
         this.nature = nature;
         this.fms = fms;
         this.farmzoid = [new Farmzoid(21, 19), new Farmzoid(17, 19), new Farmzoid(19, 21), new Farmzoid(19, 17)];
+    }
+
+    natureEffects() {
+        for(let j = 0; j < 20; j++) {
+            validPlots[i].plant.incrementAge();
+            if(nature.sky === "clear" && validPlots[i].plant.waterReserve > 0) {
+                validPlots[i].plant.waterReserve--;
+            }
+            if(nature.sky === "rainy") {
+                validPlots[i].plant.waterReserve++;
+            }
+            if(nature.sky === "cloudy") {
+                validPlots[i].plant.decrementAge();
+            }
+            if(validPlots[i].plant.waterReserve === 0) {
+                validPlots[i].plant.decrementFruitColor();
+            }
+        }
+    }
+
+    setupTasks() {
+        for(let i = 0; i < 20; i++) {
+            if(validPlots[i].hasPlot === null){
+                fms.addTasks("place plot")
+            }
+            if(validPlots[i].plant.plantType === null) {
+                fms.addTasks("plant seed");
+            }
+            if(validPlots[i].plant.fertilized === false){
+                fms.addTasks("fertilize");
+            }
+            if(validPlots[i].plant.fertilized === true && validPlots[i].plant.waterReserve <= 3) {
+                fms.addTasks("water");
+            }
+            if(validPlots[i].plant.blight === true) {
+                fms.addTasks("soaping");
+            }
+            if(validPlots[i].plant.fruitingState === "red" && validPlots[i].plant.waterReserve > 0 && validPlots[i].plant.waterReserve <=3) {
+                fms.addTasks("harvest");
+            }
+        }
     }
 }
 // End of Farm Objects
 
 
 // Farm Methods
+
+// set validPlots
+let validPlots =    [new Plot(plotLocations[0][0], plotLocations[0][1]), 
+                     new Plot(plotLocations[1][0], plotLocations[1][1]), 
+                     new Plot(plotLocations[2][0], plotLocations[2][1]), 
+                     new Plot(plotLocations[3][0], plotLocations[3][1]), 
+                     new Plot(plotLocations[4][0], plotLocations[4][1])];
+
 
 // call to change day, and also check to see if the growing season has ended
 function dayCounter(dayCount) {
